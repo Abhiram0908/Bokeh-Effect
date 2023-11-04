8597509565fc7b3a4760e5f207b87fc36ffdefed
Abhiram0908 <dacharlaabhiram0908@gmail.com>
2023-11-04 23:18:47 +0530

    working

--- Bokeh_effect.py
+++ Bokeh_effect.py
@@ -1,66 +1,118 @@
+# import cv2
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# def to_find_TKS(input_image_path):
+#     # Read the input image
+#     input_image = cv2.imread(input_image_path)
+
+#     # Check if the input image is loaded correctly
+#     if input_image is None:
+#         print("Error: Check if the input image path is correct")
+#         return
+
+#     # Convert the input image to grayscale
+#     grayscale_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+
+#     # Apply adaptive thresholding to determine the optimal threshold value
+#     threshold_value, tup = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+#     # Determine the optimal Gaussian blur kernel size and standard deviation based on the image size
+#     height, width = grayscale_image.shape
+#     # Ensure kernel size is odd and at least 3
+#     kernel_size = (max(3, int(width / 10) | 1), max(3, int(height / 10) | 1))
+#     sigma = (kernel_size[1]*0.5 - 1)*0.2
+
+#     return threshold_value, kernel_size, sigma
+
+# def perform_bokeh(input_image_path, output_image_path):
+#     # Read the input image
+#     input_image = cv2.imread(input_image_path)
+
+#     # Check if the input image is loaded correctly
+#     if input_image is None:
+#         print("Error: Check if the input image path is correct")
+#         return
+
+#     # Determine parameters based on the input image
+#     threshold_value, kernel_size, sigma = to_find_TKS(input_image_path)
+
+#     # Convert the input image to grayscale
+#     grayscale_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+
+#     # Apply binary thresholding using the determined threshold value
+#     _, binary_mask = cv2.threshold(grayscale_image, threshold_value, 255, cv2.THRESH_BINARY)
+
+#     # Invert the binary mask
+#     inverted_mask = cv2.bitwise_not(binary_mask)
+
+#     # Apply Gaussian blur to the background using the determined kernel size and sigma
+#     blurred_background = cv2.GaussianBlur(input_image, kernel_size, sigma)
+
+#     # Combine the segmented subject and blurred background
+#     input_image[inverted_mask == 255] = blurred_background[inverted_mask == 255]
+
+#     # Save or display the final image
+#     cv2.imwrite(output_image_path, input_image)
+#     plt.imshow(cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB))
+#     plt.title('Blurred')
+#     plt.show()
+
+# # Input and output image paths
+# input_image_path = 'image2.jpg'
+# output_image_path = 'output.jpg'
+
+# # Call the function to segment and blur the background
+# perform_bokeh(input_image_path, output_image_path)
+
+
+from flask import Flask, request, render_template, send_file
+from PIL import Image
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import io
 
-def to_find_TKS(input_image_path):
-    # Read the input image
-    input_image = cv2.imread(input_image_path)
+app = Flask(_name_)
 
-    # Check if the input image is loaded correctly
-    if input_image is None:
-        print("Error: Check if the input image path is correct")
-        return
-
-    # Convert the input image to grayscale
-    grayscale_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-
-    # Apply adaptive thresholding to determine the optimal threshold value
-    threshold_value, tup = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # Determine the optimal Gaussian blur kernel size and standard deviation based on the image size
+def determine_parameters(input_image):
+    grayscale_image = cv2.cvtColor(input_image, cv2.COLOR_RGB2GRAY)
+    threshold_value, _ = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     height, width = grayscale_image.shape
-    # Ensure kernel size is odd and at least 3
     kernel_size = (max(3, int(width / 10) | 1), max(3, int(height / 10) | 1))
     sigma = (kernel_size[1]*0.5 - 1)*0.2
-
     return threshold_value, kernel_size, sigma
 
-def perform_bokesh(input_image_path, output_image_path):
-    # Read the input image
-    input_image = cv2.imread(input_image_path)
-
-    # Check if the input image is loaded correctly
-    if input_image is None:
-        print("Error: Check if the input image path is correct")
-        return
-
-    # Determine parameters based on the input image
-    threshold_value, kernel_size, sigma = to_find_TKS(input_image_path)
-
-    # Convert the input image to grayscale
-    grayscale_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-
-    # Apply binary thresholding using the determined threshold value
+def apply_bokeh_effect(input_image):
+    threshold_value, kernel_size, sigma = determine_parameters(input_image)
+    grayscale_image = cv2.cvtColor(input_image, cv2.COLOR_RGB2GRAY)
     _, binary_mask = cv2.threshold(grayscale_image, threshold_value, 255, cv2.THRESH_BINARY)
-
-    # Invert the binary mask
     inverted_mask = cv2.bitwise_not(binary_mask)
-
-    # Apply Gaussian blur to the background using the determined kernel size and sigma
     blurred_background = cv2.GaussianBlur(input_image, kernel_size, sigma)
-
-    # Combine the segmented subject and blurred background
     input_image[inverted_mask == 255] = blurred_background[inverted_mask == 255]
+    return input_image
 
-    # Save or display the final image
-    cv2.imwrite(output_image_path, input_image)
-    plt.imshow(cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB))
-    plt.title('Blurred')
-    plt.show()
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# Input and output image paths
-input_image_path = 'image2.jpg'
-output_image_path = 'output.jpg'
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        image = Image.open(uploaded_file)
+        image_array = np.array(image)
 
-# Call the function to segment and blur the background
-perform_bokesh(input_image_path, output_image_path)
+        # Apply the bokeh effect
+        image_with_bokeh = apply_bokeh_effect(image_array)
+
+        image_io = io.BytesIO()
+        bokeh_image = Image.fromarray(image_with_bokeh)
+        bokeh_image.save(image_io, format='PNG')
+        image_io.seek(0)
+
+        return send_file(image_io, mimetype='image/png')
+
+    return "No file uploaded."
+
+if _name_ == '_main_':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, send_file
 import urllib.request
+from PIL import Image
 import os
+import io
 import cv2
 import numpy as np
 from werkzeug.utils import secure_filename
@@ -20,6 +22,29 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def to_find_TKS(user_image):
+    grayscale_image = cv2.cvtColor(user_image, cv2.COLOR_RGB2GRAY)
+    threshold_value, _ = cv2.threshold(
+        grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )
+    height, width = grayscale_image.shape
+    kernel_size = (max(3, int(width / 10) | 1), max(3, int(height / 10) | 1))
+    sigma = (kernel_size[1] * 0.5 - 1) * 0.2
+    return threshold_value, kernel_size, sigma
+
+
+def perform_bokeh(user_image):
+    threshold_value, kernel_size, sigma = to_find_TKS(user_image)
+    grayscale_image = cv2.cvtColor(user_image, cv2.COLOR_RGB2GRAY)
+    _, binary_mask = cv2.threshold(
+        grayscale_image, threshold_value, 255, cv2.THRESH_BINARY
+    )
+    inverted_mask = cv2.bitwise_not(binary_mask)
+    blurred_background = cv2.GaussianBlur(user_image, kernel_size, sigma)
+    user_image[inverted_mask == 255] = blurred_background[inverted_mask == 255]
+    return user_image
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -28,18 +53,32 @@ def home():
 @app.route("/", methods=["POST"])
 def upload_image():
     if "file" not in request.files:
-        flash("No file part")
+        flash("No file path")
         return render_template("uploaded_image.html")
+
     file = request.files["file"]
+
     if file.filename == "":
         flash("No image selected for uploading")
         return render_template("uploaded_image.html")
+
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-        # print('upload_image filename: ' + filename)
-        flash("Image successfully uploaded and displayed below")
-        return render_template("uploaded_image.html", filename=filename)
+        image = Image.open(file)
+        image_array = np.array(image)
+
+        # Apply the bokeh effect
+        image_with_bokeh = perform_bokeh(image_array)
+
+        image_io = io.BytesIO()
+        bokeh_image = Image.fromarray(image_with_bokeh)
+        bokeh_image.save(image_io, format="PNG")
+        image_io.seek(0)
+        
+        return send_file(image_io, mimetype='image/png')
+
+
+        # return render_template("uploaded_image.html", processed_image=image_with_bokeh)
+
     else:
         flash("Allowed image types are - png, jpg, jpeg, gif")
         return render_template("uploaded_image.html")
@@ -47,9 +86,8 @@ def upload_image():
 
 @app.route("/display/<filename>")
 def display_image(filename):
-    # print('display_image filename: ' + filename)
     return redirect(url_for("static", filename="uploads/" + filename), code=301)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
